aaa51f962cd2c9ad93666cbe4d7e3bcd490dae3e
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-19 15:50:33 +0200

    added order by

--- src/Entity/Entreprise.php
+++ src/Entity/Entreprise.php
@@ -31,7 +31,9 @@ class Entreprise
     #[ORM\Column(length: 50)]
     private ?string $ville = null;
 
+
     #[ORM\OneToMany(mappedBy: 'entreprise', targetEntity: Employe::class, orphanRemoval: true)]
+    #[ORM\OrderBy(["nom" => "ASC"])]    
     private Collection $employes;
 
     public function __construct()
