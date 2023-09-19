32a2ee25b88eaf81b75640de620e1b17b739f677
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-19 11:42:44 +0200

    more imports + route to add company

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Entreprise;
+use App\Form\EntrepriseType;
 use App\Repository\EntrepriseRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,4 +23,24 @@ class EntrepriseController extends AbstractController
             'entreprises' => $entreprises
         ]);
     }
+
+    #[Route('/entreprise/new', name: 'new_entreprise')]
+    public function new(Request $request) : Response 
+    {
+        $entreprise = new Entreprise();
+
+        $form = $this->createForm(EntrepriseType::class, $entreprise);
+        return $this->render('entreprise/new.html.twig', [
+            'formAddEntreprise' => $form
+        ]);
+    }
+
+    #[Route('/entreprise/{id}', name: 'display_app_entreprise')]
+    public function display(Entreprise $entreprise) : Response 
+    {
+        return $this->render('entreprise/display.html.twig', [
+            'entreprise' => $entreprise
+        ]);
+    }
+
 }
