06d6645282be3558f9de6413580281dd8c5e20cb
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-19 11:42:29 +0200

    route to add an employee + more imports

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Employe;
+use App\Form\EmployeType;
 use App\Repository\EmployeRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,6 +18,26 @@ class EmployeController extends AbstractController
         $employes = $employeRepository->findBy([],["nom" => "ASC"]);
         return $this->render('employe/index.html.twig', [
             'employes' => $employes,
+        ]);
+    }
+
+    #[Route('/employe/new', name: 'new_employe')]
+    public function new(Request $request) : Response 
+    {
+        $employe = new Employe();
+
+        $form = $this->createForm(EmployeType::class, $employe);
+        return $this->render('employe/new.html.twig', [
+            'formAddEmploye' => $form
+        ]);
+    }
+
+
+    #[Route('/employe/{id}', name: 'display_app_employe')]
+    public function display(Employe $employe) : Response 
+    {
+        return $this->render('employe/display.html.twig', [
+            'employe' => $employe
         ]);
     }
 }
